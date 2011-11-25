41d5a713d40f60af70fa72258eb49b3e5ab77b97
Damon Oehlman <damon.oehlman@gmail.com>
2011-11-25 17:11:08 +1000

    Added ability to close a notifier, bump version (0.0.2)

--- examples/couch.js
+++ examples/couch.js
@@ -3,7 +3,11 @@ var changeling = require('../');
 changeling.monitor('http://10.211.55.4:5984/lbs', { type: 'couchdb', getDoc: true }, function(err, notifier) {
     if (! err) {
         notifier.on('change', function(data) {
-            console.log(data.id);
+            console.log('got change id: ' + data.id);
+        });
+        
+        notifier.on('close', function() {
+            console.log('notifier closed');
         });
     }
 });
