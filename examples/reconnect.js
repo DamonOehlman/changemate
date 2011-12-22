050d54718ec679885d3b712c939f9000bbe3f824
Damon Oehlman <damon.oehlman@sidelab.com>
2011-12-22 22:34:22 +1000

    Tweaks to reconnect behaviour

---
+++ examples/reconnect.js
@@ -0,0 +1,28 @@
+var changemate = require('../'),
+    counter = 0,
+    opts = {
+        type: 'couchdb',
+        throttleDelay: 400
+    };
+    
+changemate('http://10.211.55.4:5984/lbs', opts, function(err, notifier) {
+    if (! err) {
+        notifier.on('change', function() {
+            // console.log('change');
+        });
+        
+        notifier.on('connect', function() {
+            console.log('connected, now try turning off your couch instance');
+            
+            // interrupt the connection (close the connection, but allow reconnects)
+            setTimeout(function() {
+                console.log('interrupting stream !!!');
+                notifier.close(true);
+            }, 500);
+        });
+        
+        notifier.on('close', function() {
+            console.log('notifier closed');
+        });
+    }
+});
