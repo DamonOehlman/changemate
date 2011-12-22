4efcb9552f6d08bd1088a99c2202fa88b3f7ed19
Damon Oehlman <damon.oehlman@sidelab.com>
2011-12-22 17:39:17 +1000

    Adding tests

---
+++ test/couchdb.js
@@ -0,0 +1,62 @@
+var assert = require('assert'),
+    changemate = require('../'),
+    debug = require('debug')('tests'),
+    config = require('config'),
+    nano = require('nano'),
+    // remove any trailing delimiters
+    couchUrl = config.couchurl.replace(/\/$/, ''),
+    targetUrl = couchUrl + '/' + config.db,
+    db = nano(couchUrl).use(config.db),
+    reTrailingDelimiter = /\/$/,
+    testDoc = {
+        _id: 'bob',
+        name: 'Bob',
+        age: 75
+    },
+    _updateSeq,
+    _changes = [];
+    
+describe('changemate can detect changes in a couch db', function() {
+    it('can connect to the test database', function(done) {
+        debug('getting info for database: ' + targetUrl);
+        db.info(function(err, res) {
+            if (! err) {
+                _updateSeq = res.update_seq;
+                debug('received info, setting update seq to: ' + _updateSeq);
+            }
+            
+            done(err);
+        });
+    });
+    
+    it('can configure the change monitor', function(done) {
+        changemate(targetUrl, { type: 'couchdb', since: _updateSeq }, function(err, notifier) {
+            notifier.on('change', function(item) {
+                debug('captured update for item: ' + item.id);
+                _changes.push(item);
+            });
+
+            done(err);
+        });
+    });
+
+    it('can write documents to the test database', function(done) {
+        debug('getting ' + testDoc._id + ' from the database');
+        db.get(testDoc._id, function(err, data) {
+            if (! err) {
+                testDoc._rev = data._rev;
+            }
+            
+            // update the document
+            debug('pushing update to ' + testDoc._id);
+            db.insert(testDoc, done);
+        });
+    });
+    
+    it('intercepted the change', function(done) {
+        setTimeout(function() {
+            assert(_changes.length);
+            done();
+        }, 1000);
+    });
+});
