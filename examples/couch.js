35a4915cb452d7d277871359ae4fbac59b591adc
Damon Oehlman <damon.oehlman@sidelab.com>
2011-12-23 13:02:48 +1000

    Fixed simple example

--- examples/couch.js
+++ examples/couch.js
@@ -1,6 +1,6 @@
 var changemate = require('../'),
     counter = 0,
-    notifier = changemate.watch('http://sidelab.iriscouch.com/seattle_neighbourhood');
+    notifier = changemate.watch('<:couch:> http://sidelab.iriscouch.com/seattle_neighbourhood');
     
 notifier.on('change', function(data) {
     console.log('got change id: ' + data.id + ', seq: ' + data.seq + ', counter: ' + (++counter));
