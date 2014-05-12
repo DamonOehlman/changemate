ae7712008b5231126a8ea96d4184b903c6169376
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-13 08:40:20 +1000

    Tweak formatting

--- examples/checkpoint.js
+++ examples/checkpoint.js
@@ -1,29 +1,31 @@
-var changemate = require('../'),
-    counter = 0, notifier, _checkpoint;
-    
+var changemate = require('../');
+var counter = 0;
+var notifier;
+var _checkpoint;
+
 function _createNotifier() {
-    // reset the counter
-    counter = 0;
-    
-    console.log(_checkpoint);
-    
-    // create the notifier
-    notifier = changemate.watch(
-        '<:couch:> http://sidelab.iriscouch.com/seattle_neighbourhood', // target
-        {}, // options
-        _checkpoint // checkpoint
-    );
-    
-    notifier.on('change', function(data, checkpoint) {
-        // save the checkpoint
-        _checkpoint = checkpoint;
-        
-        console.log('got change id: ' + data.id + ', seq: ' + data.seq + ', counter: ' + (++counter));
-        if (counter >= 100) {
-            notifier.close();
-            _createNotifier();
-        }
-    });
+  // reset the counter
+  counter = 0;
+
+  console.log(_checkpoint);
+
+  // create the notifier
+  notifier = changemate(
+    '<:couch:> http://sidelab.iriscouch.com/seattle_neighbourhood', // target
+    {}, // options
+    _checkpoint // checkpoint
+  );
+
+  notifier.on('change', function(data, checkpoint) {
+    // save the checkpoint
+    _checkpoint = checkpoint;
+
+    console.log('got change id: ' + data.id + ', seq: ' + data.seq + ', counter: ' + (++counter));
+    if (counter >= 100) {
+      notifier.close();
+      _createNotifier();
+    }
+  });
 }
 
 // create the notifier
