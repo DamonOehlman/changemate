242214081b2950e6d782e2fee355f87491669583
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-13 08:40:41 +1000

    Reformat and include inline docs

--- README.md
+++ README.md
@@ -1,64 +1,51 @@
-# Changemate
+# changemate
 
-<a href="http://travis-ci.org/#!/steelmesh/changemate"><img src="https://secure.travis-ci.org/steelmesh/changemate.png" alt="Build Status"></a>
-
-Changemate is a change notification service and framework. At present it only supports responding to the `_changes` feed of a couch database, but will be expanded in time to support other change notification formats.
+Changemate is a change notification service and framework. At present it only
+supports responding to the `_changes` feed of a couch database, but will be
+expanded in time to support other change notification formats.
 
 ## Supported Notifiers
 
 ### CouchDB
 
-The couchdb notifier is the first cab off the rank, as it is required for [steelmesh](https://github.com/steelmesh/steelmesh).  The couchdb notifier hooks into the [continuous changes feed](http://wiki.apache.org/couchdb/HTTP_Document_API#A_changes) for a couchdb instance.
-
-__Example Creation:__
-
-```js
-var notifier = changemate.watch('<:couch:> http://localhost:5984/testdb');
-```
-
-_OR_
+The couchdb notifier is the first cab off the rank, as it is required for
+[steelmesh](https://github.com/steelmesh/steelmesh).  The couchdb notifier hooks
+into the [continuous changes feed](http://wiki.apache.org/couchdb/HTTP_Document_API#A_changes)
+for a couchdb instance.
 
 ```js
-var notifier = changemate.watch('http://localhost:5984/testdb', { type: 'couch' });
+var changemate = require('changemate');
+var counter = 0;
+var notifier = changemate.watch('<:couch:> http://sidelab.iriscouch.com/seattle_neighbourhood');
+
+notifier.on('change', function(data) {
+  console.log('got change id: ' + data.id + ', seq: ' + data.seq + ', counter: ' + (++counter));
+});
+
+notifier.on('close', function() {
+  console.log('notifier closed');
+});
+
 ```
 
 __Supported Options:__
 
 - `since` - the change_sequence to get changes since
-- `include_docs` - whether or not document fragments should be included with the change data
-- `heartbeat` - the heartbeat interval for the changes feed
+- `include_docs` - whether or not document fragments should be included
+   with the change data
 
-### Coming Soon
+## State Management
 
-- Filesystem
+You can see by running the example above, that the changes for the feed are
+read from `since=0` each time.  While this is good for the purpose of the
+example, it's unlikely to be the desired behaviour in your application.
 
-## Examples
-
-### Simple Example
+Changemate does not offer a checkpoint persistence mechanism, but does provide
+checkpoint information so that you can store that information yourself and
+use it:
 
 ```js
 var changemate = require('changemate'),
-    counter = 0,
-    notifier = changemate.watch('<:couch:> http://sidelab.iriscouch.com/seattle_neighbourhood');
-    
-notifier.on('change', function(data) {
-    console.log('got change id: ' + data.id + ', seq: ' + data.seq + ', counter: ' + (++counter));
-});
-
-notifier.on('close', function() {
-    console.log('notifier closed');
-});
-```
-
-### State Management
-
-You can see by running the example above, that the changes for the feed are read from `since=0` each time.  While this is good for the purpose of the example, it's unlikely to be the desired behaviour in your application.
-
-Changemate does not offer a checkpoint persistence mechanism, but does provide checkpoint information so that you can store that information yourself and use it:
-
-
-```js
-var changemate = require('../'),
     counter = 0, notifier, _checkpoint;
     
 function _createNotifier() {
@@ -90,13 +77,21 @@ function _createNotifier() {
 _createNotifier();
 ```
 
-In the example above, we are collecting the checkpoint data that is passed through in the change event.  Additionally, we are closing the notifier for every 100 updates, and creating a new notifier with the checkpoint data that has been captured in previous events.
+In the example above, we are collecting the checkpoint data that is passed
+through in the change event.  Additionally, we are closing the notifier for
+every 100 updates, and creating a new notifier with the checkpoint data that
+has been captured in previous events.
 
-To make the sample above persist across sessions, the checkpoint information could be save to a local JSON file or similar and reloaded the next time the script ran to start from next required update.
+To make the sample above persist across sessions, the checkpoint information
+could be save to a local JSON file or similar and reloaded the next time the
+script ran to start from next required update.
 
 ## Alternative Solutions
 
-Changemate is designed to be a change notification library that supports providing notifications from various sources.  There are some excellent (and more mature) libraries that provide similar services for usually just one of the change sources:
+Changemate is designed to be a change notification library that supports
+providing notifications from various sources.  There are some excellent
+(and more mature) libraries that provide similar services for usually
+just one of the change sources:
 
 ### CouchDB
 
--- index.js
+++ index.js
@@ -2,14 +2,72 @@ var debug = require('debug')('changemate'),
     util = require('util'),
     events = require('events'),
     errorCodes = {
-        NOT_PERMITTED: 'NOT PERMITTED'
+      NOT_PERMITTED: 'NOT PERMITTED'
     },
     reType = /^\<\:(\w+)\:\>\s*?(.*)/,
     _monitor,
     aliases = {
-        'couchdb': 'couch'
+      'couchdb': 'couch'
     };
 
+
+/**
+  # changemate
+
+  Changemate is a change notification service and framework. At present it only
+  supports responding to the `_changes` feed of a couch database, but will be
+  expanded in time to support other change notification formats.
+
+  ## Supported Notifiers
+
+  ### CouchDB
+
+  The couchdb notifier is the first cab off the rank, as it is required for
+  [steelmesh](https://github.com/steelmesh/steelmesh).  The couchdb notifier hooks
+  into the [continuous changes feed](http://wiki.apache.org/couchdb/HTTP_Document_API#A_changes)
+  for a couchdb instance.
+
+  <<< examples/couch.js
+
+  __Supported Options:__
+
+  - `since` - the change_sequence to get changes since
+  - `include_docs` - whether or not document fragments should be included
+     with the change data
+
+  ## State Management
+
+  You can see by running the example above, that the changes for the feed are
+  read from `since=0` each time.  While this is good for the purpose of the
+  example, it's unlikely to be the desired behaviour in your application.
+
+  Changemate does not offer a checkpoint persistence mechanism, but does provide
+  checkpoint information so that you can store that information yourself and
+  use it:
+
+  <<< examples/checkpoint.js
+
+  In the example above, we are collecting the checkpoint data that is passed
+  through in the change event.  Additionally, we are closing the notifier for
+  every 100 updates, and creating a new notifier with the checkpoint data that
+  has been captured in previous events.
+
+  To make the sample above persist across sessions, the checkpoint information
+  could be save to a local JSON file or similar and reloaded the next time the
+  script ran to start from next required update.
+
+  ## Alternative Solutions
+
+  Changemate is designed to be a change notification library that supports
+  providing notifications from various sources.  There are some excellent
+  (and more mature) libraries that provide similar services for usually
+  just one of the change sources:
+
+  ### CouchDB
+
+  - [IrisCouch Follow](https://github.com/iriscouch/follow)
+
+**/
 /* internals */
 
 /**
@@ -19,100 +77,100 @@ var debug = require('debug')('changemate'),
  * the options contain a target definition, the type specified in the options takes precedence
  */
 function guessType(target, opts) {
-    var match = reType.exec(target);
-    if (match) {
-        opts.type = opts.type || match[1];
-        target = match[2];
-    }
+  var match = reType.exec(target);
+  if (match) {
+    opts.type = opts.type || match[1];
+    target = match[2];
+  }
 
-    // if the type is definied, and matches an alias remap to the actual type
-    opts.type = aliases[opts.type] ||  opts.type;
+  // if the type is definied, and matches an alias remap to the actual type
+  opts.type = aliases[opts.type] ||  opts.type;
 
-    // return the potentially modified target string
-    return target;
+  // return the potentially modified target string
+  return target;
 } // _guessType
 
 function setupReconnector(notifier, opts) {
 
-    var failCount = 0;
+  var failCount = 0;
 
-    function reconnect() {
-        notifier.reconnect(opts);
-    }
+  function reconnect() {
+    notifier.reconnect(opts);
+  }
 
-    // check if we have a reconnect delay specified
-    if (opts.reconnectDelay) {
-        notifier.on('error', function(err) {
-            if (err !== errorCodes.NOT_PERMITTED) {
-                debug('error detected, attempting reconnection in ' + opts.reconnectDelay + 'ms');
-                setTimeout(reconnect, opts.reconnectDelay);
-            }
-        });
+  // check if we have a reconnect delay specified
+  if (opts.reconnectDelay) {
+    notifier.on('error', function(err) {
+      if (err !== errorCodes.NOT_PERMITTED) {
+        debug('error detected, attempting reconnection in ' + opts.reconnectDelay + 'ms');
+        setTimeout(reconnect, opts.reconnectDelay);
+      }
+    });
 
-        // when the notifier closes, attempt a reconnection
-        notifier.on('close', function() {
-            debug('notifier close detected, attempting reconnection in ' + opts.reconnectDelay + 'ms');
-            setTimeout(reconnect, opts.reconnectDelay);
-        });
-    }
+    // when the notifier closes, attempt a reconnection
+    notifier.on('close', function() {
+      debug('notifier close detected, attempting reconnection in ' + opts.reconnectDelay + 'ms');
+      setTimeout(reconnect, opts.reconnectDelay);
+    });
+  }
 } // setupReconnector
 
 function createNotifier(type, target, opts, checkpoint) {
-    var lib, notifier;
+  var lib, notifier;
 
-    if (! type) {
-        throw new Error('Unable to create notifier of undefined type');
-    }
+  if (! type) {
+    throw new Error('Unable to create notifier of undefined type');
+  }
 
+  try {
+    lib = require('changemate-' + type);
+  }
+  catch (e) {
     try {
-        lib = require('changemate-' + type);
+      lib = require('./' + type);
     }
     catch (e) {
-        try {
-            lib = require('./' + type);
-        }
-        catch (e) {
-            console.log('could not require ./' + type);
-        }
+      console.log('could not require ./' + type);
     }
+  }
 
-    if (! lib) {
-        throw new Error('Unable to create changemate notifier of type \'' + type + '\'');
+  if (! lib) {
+    throw new Error('Unable to create changemate notifier of type \'' + type + '\'');
+  }
+
+  if (lib && lib.Notifier) {
+    // normalize the target
+    target = lib.normalizeTarget ? lib.normalizeTarget(target) : target;
+
+    // create the notifier
+    notifier = new lib.Notifier(target, opts);
+    notifier.type = type;
+
+    // make the request to couch
+    notifier.connect(target, opts);
+
+    // if we should auto reconnect, then handle that here
+    if (notifier.reconnect) {
+      setupReconnector(notifier, opts);
     }
+  }
 
-    if (lib && lib.Notifier) {
-        // normalize the target
-        target = lib.normalizeTarget ? lib.normalizeTarget(target) : target;
-
-        // create the notifier
-        notifier = new lib.Notifier(target, opts);
-        notifier.type = type;
-
-        // make the request to couch
-        notifier.connect(target, opts);
-
-        // if we should auto reconnect, then handle that here
-        if (notifier.reconnect) {
-            setupReconnector(notifier, opts);
-        }
-    }
-
-    return notifier;
+  return notifier;
 };
 
 function changemate(target, opts) {
-    // ensure we have valid opts
-    opts = opts || {};
-    
-    // guess the notifier type
-    target = guessType(target, opts);
-    
-    // initialise the reconnect delay
-    opts.reconnectDelay = typeof opts.reconnectDelay != 'undefined' ? opts.reconnectDelay : 5000;
-    
-    // create the notifier
-    return createNotifier(opts.type, target, opts);
-    
+  // ensure we have valid opts
+  opts = opts || {};
+
+  // guess the notifier type
+  target = guessType(target, opts);
+
+  // initialise the reconnect delay
+  opts.reconnectDelay = typeof opts.reconnectDelay != 'undefined' ? opts.reconnectDelay : 5000;
+
+  // create the notifier
+  return createNotifier(opts.type, target, opts);
+
 }
 
 // attach the aliases
